d31cd76eb56dd3c3c9f66369548f2bc5eb112442
danielapochini <daniela.pochini@bs2tecnologia.com.br>
2022-03-22 12:36:22 -0300

    Interceptando API

---
+++ cypress/integration/TestApi.spec.js
@@ -0,0 +1,27 @@
+describe('API Test Suite', () => {
+    beforeEach(() => {
+        cy.visit("/angularAppdemo/")
+    })
+
+    it('Intercepting Request', () => {
+        cy.intercept(
+            {
+                method: 'GET',
+                url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+            },
+            {
+                statusCode: 200,
+                body:
+                    [
+                        {
+                            "book_name": "Teste Dan",
+                            "isbn": "RSU",
+                            "aisle": "2301"
+                        }
+                    ]
+            }).as('bookRetrievals')
+        cy.get('.btn-primary').click()
+        cy.wait('@bookRetrievals')
+        cy.get('p').should('have.text', 'Oops only 1 Book available')
+    })
+})
